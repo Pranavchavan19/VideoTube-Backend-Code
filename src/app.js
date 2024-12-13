c7f107118ecd9aeaeff56af3282982cdd11b5909
Pranavbc <pranav1952000@gmail.com>
2024-12-13 15:18:45 +0530

    np

--- src/app.js
+++ src/app.js
@@ -19,17 +19,22 @@ const config = {
     }
   };
   
+  const corsOptions = {
+    origin: 'https://video-tube-frontend-code.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  };
 //   const cors = require('cors');
-const corsOptions ={
-    origin:'https://video-tube-backend-code.vercel.app/', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
+// const corsOptions ={
+//     origin:'https://video-tube-backend-code.vercel.app', 
+//     credentials:true,            //access-control-allow-credentials:true
+//     optionSuccessStatus:200
+// }
 
 app.use(cors(corsOptions));
   const {
     data: { ip }
-  } = await axios.get("https://video-tube-backend-code.vercel.app/", config);
+  } = await axios.get("https://video-tube-backend-code.vercel.app", config);
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
